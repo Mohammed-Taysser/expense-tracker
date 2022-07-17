4929fcca5cc342023f71e5d540befa80cf208e78
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-17 22:41:43 +0200

    create transaction feature

---
+++ src/redux/features/Transactions.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const TransactionSlice = createSlice({
+	name: 'transactions',
+	initialState: {
+		expense: [],
+		balance: 0,
+	},
+	reducers: {
+		initTransaction: (state) => {
+			const newState =
+				JSON.parse(localStorage.getItem('transactions') || '') || [];
+			state.expense = newState;
+			state.balance = calculateBalance(newState);
+		},
+		addTransaction: (state, action) => {
+			const newState = [...state.expense, action.payload];
+			state.expense = newState;
+			state.balance = calculateBalance(newState);
+			localStorage.setItem('transactions', JSON.stringify(newState));
+		},
+		deleteTransaction: (state, action) => {
+			const newState = state.expense.filter(
+				(transaction) => transaction.id !== action.payload
+			);
+			state.expense = newState;
+			state.balance = calculateBalance(newState);
+			localStorage.setItem('transactions', JSON.stringify(newState));
+		},
+	},
+});
+
+/**
+ * calculate transaction total balance
+ * @param {Array} arr Transaction Array
+ * @returns {Number} calculated Balance
+ */
+function calculateBalance(arr) {
+	return arr.reduce(
+		(prev, value) =>
+			value.type === 'expense' ? prev - value.amount : prev + value.amount,
+		0
+	);
+}
+
+// Action creators are generated for each case reducer function
+export const { initTransaction, addTransaction, deleteTransaction } =
+	TransactionSlice.actions;
+
+export default TransactionSlice.reducer;
