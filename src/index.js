092e25bee7017cb80263aa677d5cf3ebc3b4540e
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-07-17 22:40:04 +0200

    add speechly provider & core.scss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './__tests__/reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { SpeechProvider } from '@speechly/react-client';
+import './assets/scss/core.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<SpeechProvider
+			language='en-US'
+			appId='74d530fe-d28a-40e7-8768-012e3f2af4df'
+		>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</SpeechProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
